9dccffb6b89c7a69919465696077a57e99f832a9
Luiz Filipe <pradoluizf@gmail.com>
2018-09-02 13:51:37 +0000

    Revert "Comentando linha da Canario"
    This reverts commit 12d4867f48694d1082d40711ce367ac06fc826a8.

--- canario_negro.sh
+++ canario_negro.sh
@@ -5,4 +5,4 @@ echo "Script da Canario"
 cowsay -f tux $(fortune)
 
 ping -c2 192.168.99.20 >- && echo "Com conexão" || echo "Sem conexão"
-#ping -c2 192.168.99.99 >- && echo "Com conexão" || echo "Sem conexâo"
+ping -c2 192.168.99.99 >- && echo "Com conexão" || echo "Sem conexâo"
